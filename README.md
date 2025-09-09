6993e277e6d6772e3bca8e4dbc0e0613d1e0ea0b
Shameel Mohanlal <shameelmohanlal@gmail.com>
2025-09-09 22:23:32 +0200

    Create dummy users

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # fastapi-onboarding-project
+
+### Dummy users
+Super User: 
+
+* `bob@test.com`
+* `secretpw`
+
+User: 
+
+* `tom@test.com`
+* `secretpw2`
--- app/main.py
+++ app/main.py
@@ -1,14 +1,33 @@
 from fastapi import FastAPI
 from fastapi.routing import APIRoute
 
-from app.core.db import create_db_and_tables
+from app.core.db import engine, create_db_and_tables
 from app.api.main import api_router
+
+from app import crud, models
+from sqlmodel import Session, select
+
+def startup_populate_users():
+    with Session(engine) as session:
+        # Check if users already exist
+        statement = select(models.User)
+        results = session.exec(statement).first()
+        if results:
+            return # Skip if users exist
+
+        dummy_users = [
+            models.UserCreate(email="bob@test.com", is_active=True, is_superuser=True, full_name="Bob Jones", password="secretpw"),
+            models.UserCreate(email="tom@test.com", is_active=True, is_superuser=False, full_name="Tom Locke", password="secretpw2")
+        ]
+        for user in dummy_users:
+            crud.create_user(session=session, user_create=user)
 
 app = FastAPI()
 
 @app.on_event("startup")
 def on_startup():
     create_db_and_tables()
+    startup_populate_users()
 
 @app.get("/")
 async def root():
